97ebe5866bb4afb0ca08036eae43916550b70359
RenatoStosl <renatosantoslourenco@gmail.com>
2024-03-30 18:44:07 -0300

    commit inicial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+remotePatterns: [
+    {
+        protocol: 'https',
+        domain: 'dogsapi.origamid.dev',
+    }
+]
+    }
 
-export default nextConfig;
+}
