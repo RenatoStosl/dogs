3d5361861c491df84f03323ad1b239ac2ea25d6b
RenatoStosl <renatosantoslourenco@gmail.com>
2024-04-16 10:41:03 -0300

    Refactor photos-get.ts, conta/page.tsx, and feed.tsx files

--- src/actions/photos-get.ts
+++ src/actions/photos-get.ts
@@ -13,26 +13,29 @@ export type Photo = {
     idade: string;
     acessos: string;
     total_comments: string;
-};
-
-type PhotosGetParams = {
+  };
+  
+  type PhotosGetParams = {
     page?: number;
     total?: number;
     user?: 0 | string;
-};
-
-export default async function photosGet({page= 1, total= 6, user= 0}: PhotosGetParams = {}) {
+  };
+  
+  export default async function photosGet(
+    { page = 1, total = 6, user = 0 }: PhotosGetParams = {},
+    optionsFront?: RequestInit,
+  ) {
     try {
-    const {url} = PHOTOS_GET({page, total, user})
-    const response = await fetch(url,
-    {
-        next: {revalidate: 10, tags: ['photos']},
+      const options = optionsFront || {
+        next: { revalidate: 10, tags: ['photos'] },
+      };
+      const { url } = PHOTOS_GET({ page, total, user });
+      const response = await fetch(url, options);
+      if (!response.ok) throw new Error('Erro ao pegar as fotos.');
+      const data = (await response.json()) as Photo[];
+      return { data, ok: true, error: '' };
+    } catch (error) {
+      return apiError(error);
     }
-    );
-    if(!response.ok) throw new Error("Erro ao buscar as fotos")
-    const data = await response.json() as Photo[];
-    return {data, ok: true, error: ""};
-} catch (error) {
-    return apiError(error);
-}
-}
+  }
+  
--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -5,26 +5,32 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-    title: "Minha Conta",
-}
-
-export default async function ContaPage() {
-    const { data: user } = await userGet()
-    const { data } = await photosGet({ user: user?.username })
+    title: 'Minha Conta',
+  };
+  
+  export default async function ContaPage() {
+    const { data: user } = await userGet();
+    const { data } = await photosGet({ user: user?.username });
     return (
-        <section>
-            {data?.length ? (< Feed photos={data} /> 
-            ):(
-            <div>
-                <p className="text-[#444] mb-4 text-xl">Nenhuma foto encontrada
-                </p>
-                    <Link href={"conta/postar"} className="inline-block text-base font-[var(--type-first)] cursor-pointer border-none rounded-[0.4rem] bg-[#fb1] text-[#764701] min-w-[8rem] py-[0.8rem] px-[1.2rem] box-border transition-all duration-100 focus:outline-none focus:ring-[3px] focus:ring-[#fea] focus:ring-offset-[4px] focus:ring-offset-[#fb1] hover:outline-none hover:ring-[3px] hover:ring-[#fea] hover:ring-offset-[4px] hover:ring-offset-[#fb1] .button:disabled {
-            opacity: 0.5;
-            cursor: wait;
-          }">
-                        Postar Foto
-                        </Link></div>
-            )}
-        </section>
+      <section>
+        {data?.length ? (
+          <Feed photos={data} user={user?.username} />
+        ) : (
+          <div>
+            <p
+              style={{ color: '#444', fontSize: '1.25rem', marginBottom: '1rem' }}
+            >
+              Nenhuma foto encontrada.
+            </p>
+            <Link
+              href={'/conta/postar'}
+              className="button"
+              style={{ display: 'inline-block' }}
+            >
+              Postar Foto
+            </Link>
+          </div>
+        )}
+      </section>
     );
-}
+  }
