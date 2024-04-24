587583ac24fbc143bc4a448f71a5c36a1c2b6f47
RenatoStosl <renatosantoslourenco@gmail.com>
2024-04-24 10:03:51 -0300

    Add jose dependency and verifyToken function

---
+++ src/functions/verify-token.ts
@@ -0,0 +1,18 @@
+import { jwtVerify } from "jose"
+
+export default async function verifyToken(token: string): Promise<boolean> {
+  try {
+    if (!token) return false
+    // try {
+    //     await jwtVerify(
+    //         token,
+    //         new TextEncoder().encode(process.env.JWT_SECRET || ''),
+    //     {
+    //         algorithms: ['HS256'],
+    //     },)
+        return true
+        }
+    catch (error) {
+        return false
+        }
+}
